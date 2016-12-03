cc1499a3d470b70ea263352f1f0dac8932ee8e6b
yoshimune <tolth002@gmail.com>
2016-12-03 08:13:40 +0000

    編集・削除機能追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @message = Message.new
@@ -19,11 +20,29 @@ class MessagesController < ApplicationController
     
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
   
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
   
+  def set_message
+    @message = Message.find(params[:id])
+  end
   
 end
